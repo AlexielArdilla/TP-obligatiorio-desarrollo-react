2163f626f76db74be6382906acc767f2274796ec
Alejandro G. Vera <eldiosvolador@hotmail.com>
2023-05-03 00:39:12 -0300

    agregué react forms

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,16 +1,47 @@
+import { useForm } from "react-hook-form";
 
-function Login(){
 
-  return(
-  <div>
+function Login() {
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ mode: "onChange" });
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
+  return (
+    <div>
       <h1>Login</h1>
-      <form>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <label>Usuario</label><br />
-        <input required /><br />
-        <label>Password</label><br />
-        <input type="password" required /><br />
-        <br />
-        <button>Enviar</button>
+        <input type="text" {...register("usuario", { required: true })} /><br />
+        <label>Contraseña</label> <br />
+        <input
+          type="password"
+          {...register("password", {
+            required: true,
+            minLength: 6,
+            maxLength: 12,
+          })} required
+        /> <br />
+        {errors.password && (
+          <>
+            {errors.password?.type === "required" && (
+              <span>El campo es obligatorio</span>
+            )}
+            {errors.password?.type === "minLength" && (
+              <span>Debe completar al menos 6 caracteres</span>
+            )}
+            {errors.password?.type === "maxLength" && (
+              <span>Debe completar menos de 12 caracteres</span>
+            )}
+          </>
+        )} <br />
+        <button type="submit">Enviar</button>
       </form>
     </div>);
 }
--- src/Pages/Registro.jsx
+++ src/Pages/Registro.jsx
@@ -1,24 +1,83 @@
+import { useForm } from "react-hook-form";
+
+
 function Registro() {
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ mode: "onChange" });
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
   return (
     <div>
-      <h1>Registrarse</h1>
-      <form>
-        <label>Nombre Completo</label><br />
-        <input required /><br />
-        <label >Nombre de Usuario</label><br />
-        <input required /><br />
-        <label>Email</label><br />
-        <input type="email" required /><br />
-        <label>Password</label><br />
-        <input type="password" required /><br />
-        <label>Confirmar password</label><br />
-        <input type="password" required /><br />
-        <br />
-        <button>Enviar</button>
-      </form>
-    </div>
-  );
+      <form onSubmit={handleSubmit(onSubmit)}>
 
+        <label>Nombre</label> <br />
+        <input type="text" {...register("nombre", { required: true })} />
+        {errors.nombre && (
+
+          <span>El campo es obligatorio</span>
+
+        )} <br />
+
+        <label>Apellido</label> <br />
+        <input type="text" {...register("apellido")} />
+
+        <br />
+        <label>Email</label> <br />
+        <input
+          type="text"
+          {...register("email", {
+            required: true,
+            pattern:
+              /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/i,
+          })}
+        />
+        <br />
+        {errors.email && (
+          <>
+            {errors.email?.type === "required" && (
+              <span>El campo es obligatorio</span>
+            )}
+            {errors.email?.type === "pattern" && (
+              <span>Formato email no valido</span>
+            )}
+          </>
+        )}
+        <br />
+        <label>Contraseña</label> <br />
+        <input
+          type="password"
+          {...register("password", {
+            required: true,
+            minLength: 6,
+            maxLength: 12,
+          })} required
+        /> <br />
+        {errors.password && (
+          <>
+            {errors.password?.type === "required" && (
+              <span>El campo es obligatorio</span>
+            )}
+            {errors.password?.type === "minLength" && (
+              <span>Debe completar al menos 6 caracteres</span>
+            )}
+            {errors.password?.type === "maxLength" && (
+              <span>Debe completar menos de 12 caracteres</span>
+            )}
+          </>
+        )} <br />
+
+        <button type="submit">Registrarse</button>
+      </form>
+
+    </div >
+  );
 }
 
 export default Registro;
